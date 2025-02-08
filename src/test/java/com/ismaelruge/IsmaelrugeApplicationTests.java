3ac8c34fbdfa6f756e9fd7c711c761f10fe70af2
Ismael Ruge González <ismael.ruge@ithealth.co>
2025-02-08 11:41:02 -0500

    Parcial 1

---
+++ src/test/java/com/ismaelruge/IsmaelrugeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismaelruge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsmaelrugeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
